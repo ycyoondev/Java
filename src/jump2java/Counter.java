23fda2be91895aa5dff53c56bb566bca188d470a
YC Yoon <z20152002@gmail.com>
2021-12-26 20:25:24 +0900

    Apply gitignore

---
+++ src/jump2java/Counter.java
@@ -0,0 +1,20 @@
+package jump2java;
+class Updater {
+//    public void update(int count) {
+//        count++;
+//    }
+    public void update(Counter counter) {
+        counter.count++;
+    }
+}
+public class Counter {
+    int count = 0;
+    public static void main(String[] args) {
+        Counter myCounter = new Counter();
+        System.out.println("before update:"+myCounter.count);
+        Updater myUpdater = new Updater();
+        myUpdater.update(myCounter);
+        System.out.println("after update:"+myCounter.count);
+    }
+
+}
